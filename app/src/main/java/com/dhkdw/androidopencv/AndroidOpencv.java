8e92caf21f09fcf5a9de455d0db53b9a0f7d0914
solhwap <solhwa8@gmail.com>
2021-06-27 10:50:23 +0900

    수정

--- app/src/main/java/com/dhkdw/androidopencv/AndroidOpencv.java
+++ app/src/main/java/com/dhkdw/androidopencv/AndroidOpencv.java
@@ -3,7 +3,6 @@ package com.dhkdw.androidopencv;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
 import android.content.res.Configuration;
 import android.media.AudioManager;
 import android.os.Bundle;
